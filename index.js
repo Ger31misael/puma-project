1d6dde710804b3fb0614b3517e25ca99965584da
Gerson Funez <gersonfunez43@gmail.com>
2021-05-01 21:52:21 -0600

    prueba 2

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./dist/frontend'));
+
+app.get('/*', function (req, res){
+    res.sendFile('index.html', {
+        root: 'dist/frontend'
+    });
+});
+
+app.get('/servicio-rest', (req, res) =>{
+    return res.send('Metodo get');
+});
+
+app.listen(process.env.PORT || 8081);
